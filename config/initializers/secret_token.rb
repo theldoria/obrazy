a66e0f3fa5ef009588c64af7238bac392365b32d
Frank Roland <meet_thrall@freenet.de>
2012-01-22 11:48:00 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obrazy::Application.config.secret_token = '1e7e925f8a35f9061ae3ebbd617c7420bec0f6f1564cecd2243a43b0858768306178213a23f2354a4a70981543543838155c1dc095ed8ec9a61daf1fd26967be'
